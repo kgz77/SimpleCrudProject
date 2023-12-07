43adeed93c3568630224c282edc32d31d4f4304a
Nurbekov Aidar <nurbekov741@gmail.com>
2023-12-07 16:57:59 +0600

    Initial commit

---
+++ FirstProjectMVC/Controllers/CategoryController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstProjectMVC.Data;
+using FirstProjectMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstProjectMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CategoryController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategory = _dbContext.Categories;
+            return View(objCategory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder can't be exactly match be the name");
+                TempData["error"] = "The DisplayOrder can't be exactly match be the name";
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category created successafully";
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+
+        public IActionResult Edit(int ?id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _dbContext.Categories.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder can't be exactly match be the name");
+            }
+
+
+            if (ModelState.IsValid)
+            { 
+
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category updated successafully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _dbContext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int ?id)
+        {
+            var obj = _dbContext.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Categories.Remove(obj);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Category deleted successafully";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ FirstProjectMVC/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FirstProjectMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstProjectMVC.Data
+{
+	public class AppDbContext: DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+		{
+		}
+
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
